af9c641ee70b55a9c14601b95b4d2f01a8a1cc89
acriado-otc <161324154+acriado-otc@users.noreply.github.com>
2024-09-05 09:01:20 +0200

    Initial Commit

---
+++ Hello World - MKR Wifi 101/include/arduino_secrets.h
@@ -0,0 +1,11 @@
+// Fill in  your WiFi networks SSID and password
+#define SECRET_SSID "MIWIFI_SSID"
+#define SECRET_PASS "XXXXXX"
+
+#define SECRET_BROKER "a21nf7ui6d7k9-ats.iot.eu-west-3.amazonaws.com"
+
+// Fill in the boards public certificate
+const char SECRET_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)";
